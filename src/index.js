9d0fb02f465b1fbcbbd1b5a5a58ccfd0d2ab39f4
steven victor <chikodi543@gmail.com>
2019-04-24 19:53:14 +0100

    Fetch video from youtube

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {  Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const App = () =>  {
-  return <div>Hi!</div>;
+const api_key = "AIzaSyBDCMBzPEFs93h_ZdFFqrvpqkxJ654S2ds";
+
+
+class  App extends Component  {
+
+  state = {
+    videos: []
+  }
+
+  componentDidMount() {
+    YTSearch({ key: api_key, term: 'stuffs'}, (videos) => {
+      // console.log(videos)
+      this.setState({ videos })
+      console.log(this.state.videos)
+    })
+  }
+  
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
